43a0a4d2d5f9b282bcba3773450e3c8467f79820
Boris Goncharov <boris.goncharov@netcracker.com>
2020-05-04 18:44:59 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'widgets/transaction_list.dart';
+import 'widgets/new_transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Chart!'),
+              elevation: 5,
+            ),
+          ),
+          NewTransaction(),
+          TransactionList(),
+        ],
+      ),
+    );
+  }
+}
